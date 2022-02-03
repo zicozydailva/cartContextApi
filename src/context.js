3aa2d7a325b816cd943ad42c90297ec9a6591ac7
Ogbu Ezekiel Ike <zico@Macbooks-MacBook-Pro.local>
2022-02-03 23:41:39 +0100

    implemnt full app structure, remove, clearCart func

---
+++ src/context.js
@@ -0,0 +1,52 @@
+import React, { useState, useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-useReducer-cart-project";
+const AppContext = React.createContext();
+
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({type: "CLEAR_CART"})
+  }
+
+  const increment = id => {
+    dispatch({type: "INCREASE", payload: id})
+  }
+  const decrement = id => {
+    
+  }
+
+  const remove = id => {
+    dispatch({type: "REMOVE", payload: id})
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearCart,
+        remove,
+        increment,
+        decrement,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+
+const reducer = (state,action) => {
+    if(action.type === "CLEAR_CART") {
+        return {...state, cart: []}
+    }
+    if(action.type === "REMOVE") {
+        return {
+            ...state,
+            cart: state.cart.filter(item => item.id !== action.payload)
+        }
+    }
+
+    return state;
+}
+
+export default reducer
